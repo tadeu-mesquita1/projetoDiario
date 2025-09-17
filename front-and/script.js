fd77d273f61229c38e960501ad68ba763bfd1470
Tadeu De Mesquita Rodrigues <tadeu.m.rodrigues@outlook.com>
2025-09-17 16:17:59 -0300

    começo do javascript

---
+++ front-and/script.js
@@ -0,0 +1,8 @@
+ const resultadoCadastro = document.getElementById("resultado") 
+
+ const formulario = document.getElementById("formCadastro")
+
+ formulario.addEventListener("submit",(evento)=>{
+    evento.preventDefault();
+
+ })
