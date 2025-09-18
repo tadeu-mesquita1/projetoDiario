1fb7fae3e4a4dcc58f9d6e60c869d71bb943901f
tadeu-mesquita1 <tadeu.m.rodrigues@outlook.com>
2025-09-18 19:15:00 -0300

    ainda analisando js

--- front-and/script.js
+++ front-and/script.js
@@ -1,9 +1,16 @@
- const resultadoCadastro = document.getElementById("resultado") 
+const resultadoCadastro = document.querySelectorAll("resultados")
+const formulario = document.querySelectorAll("form")
 
- const formulario = document.getElementById("formCadastro")
+formulario.forEach((form)=>{
+   form.addEventListener("submit", formularios)
+})
 
- formulario.addEventListener("submit",(evento)=>{
-    evento.preventDefault();
-    resultadoCadastro.style.display ="block"
-
- })
+function formularios(evento){
+   const envioFormulario = evento.target
+   const inputLogin = envioFormulario.querySelector("input[name = nome]")
+   const inputLoginSenha = envioFormulario.querySelector("input[name = senha]")
+   const inputCadastro = envioFormulario.querySelector("input[name = nomecadastro]")
+   const inputCadastroSenha = envioFormulario.querySelector("input[name = criasenha]")
+   const inputRecupera = envioFormulario.querySelector("input[name = nomeRecup]")
+   const inputRecuperaSenha = envioFormulario.querySelector("input[name = novaSenha]")
+}
