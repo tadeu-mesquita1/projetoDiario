3c044fe416c932673b78dca7e71770d5fd37d400
Tadeu De Mesquita Rodrigues <tadeu.m.rodrigues@outlook.com>
2025-09-19 12:14:27 -0300

    Continuação da logica do JavaScript

--- front-and/script.js
+++ front-and/script.js
@@ -1,14 +1,16 @@
- const resultadoCadastro = document.getElementById("resultado") 
-
- const formulario = document.querySelectorAll("form")
+const resultadoCadastro = document.querySelectorAll("resultados")
+const formulario = document.querySelectorAll("form")
 
 formulario.forEach((form)=>{
-   form.addEventListener("submit",Formularios)
-
+   form.addEventListener("submit", formularios)
 })
 
-
-function Formularios(evento){
-   evento.preventDefault()
- }
- 
+function formularios(evento){
+   const envioFormulario = evento.target
+   const inputLogin = envioFormulario.querySelector("input[name = nome]")
+   const inputLoginSenha = envioFormulario.querySelector("input[name = senha]")
+   const inputCadastro = envioFormulario.querySelector("input[name = nomecadastro]")
+   const inputCadastroSenha = envioFormulario.querySelector("input[name = criasenha]")
+   const inputRecupera = envioFormulario.querySelector("input[name = nomeRecup]")
+   const inputRecuperaSenha = envioFormulario.querySelector("input[name =novaSenha]" )
+}
